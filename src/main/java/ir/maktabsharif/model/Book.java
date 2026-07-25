4268c2f097f09281161786ed71e24319367922fc
beykod <alirafibeyk@gmail.com>
2026-07-25 21:24:34 +0330

    feat: Add fields to model

--- src/main/java/ir/maktabsharif/model/Book.java
+++ src/main/java/ir/maktabsharif/model/Book.java
@@ -1,4 +1,13 @@
 package ir.maktabsharif.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
+    @Id
+    private Long id;
+    private String title;
+    private String author;
+    private Integer availableCopies;
 }
