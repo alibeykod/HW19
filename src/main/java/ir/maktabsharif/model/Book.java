9729db30d868607e8f14bf3853c360c6b7eebaba
beykod <alirafibeyk@gmail.com>
2026-07-25 21:27:30 +0330

    feat: Add getters and setters methods to Book

--- src/main/java/ir/maktabsharif/model/Book.java
+++ src/main/java/ir/maktabsharif/model/Book.java
@@ -26,4 +26,36 @@ public class Book {
 
     public Book() {
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Integer getAvailableCopies() {
+        return availableCopies;
+    }
+
+    public void setAvailableCopies(Integer availableCopies) {
+        this.availableCopies = availableCopies;
+    }
 }
