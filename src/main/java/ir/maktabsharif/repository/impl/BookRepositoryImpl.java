7327ab732686c6e36c58c0dd5aa3cfb9c9403d75
beykod <alirafibeyk@gmail.com>
2026-07-27 03:18:32 +0330

    feat: Add findAll method to BookRepositoryImpl

--- src/main/java/ir/maktabsharif/repository/impl/BookRepositoryImpl.java
+++ src/main/java/ir/maktabsharif/repository/impl/BookRepositoryImpl.java
@@ -3,10 +3,12 @@ package ir.maktabsharif.repository.impl;
 import ir.maktabsharif.exception.DatabaseOperationException;
 import ir.maktabsharif.model.Book;
 import ir.maktabsharif.repository.BookRepositoryInterface;
+import ir.maktabsharif.util.DatabaseConfig;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 
+import java.util.List;
 import java.util.Optional;
 
 public class BookRepositoryImpl implements BookRepositoryInterface {
@@ -56,24 +58,39 @@ public class BookRepositoryImpl implements BookRepositoryInterface {
             tx.begin();
             em.remove(findById(id));
             tx.commit();
-        }catch (Exception e){
+        } catch (Exception e) {
             tx.rollback();
-            throw new DatabaseOperationException("Book did not Deleted !"+ e.getMessage());
+            throw new DatabaseOperationException("Book did not Deleted !" + e.getMessage());
         }
     }
 
     @Override
-    public Optional<Book>  findById(Long id) {
+    public Optional<Book> findById(Long id) {
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
-        try (em){
+        try (em) {
             tx.begin();
-            Optional<Book> book = Optional.ofNullable(em.find(Book.class , id));
+            Optional<Book> book = Optional.ofNullable(em.find(Book.class, id));
             tx.commit();
             return book;
-        }catch (Exception e){
+        } catch (Exception e) {
             tx.rollback();
-            throw new DatabaseOperationException("Book not Found"+ e.getMessage());
+            throw new DatabaseOperationException("Book not Found" + e.getMessage());
+        }
+    }
+
+    @Override
+    public List<Book> findAll() {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        try (em) {
+            tx.begin();
+            List<Book> books = em.createQuery("SELECT b FROM Book b", Book.class).getResultList();
+            tx.commit();
+            return books;
+        } catch (Exception e) {
+            tx.rollback();
+            throw new DatabaseOperationException("Error fetching all books: " + e.getMessage());
         }
     }
 }
